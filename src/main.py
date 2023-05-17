406de88d659f74723c4ab2db38ff25ec41beec43
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2023-05-17 15:53:53 -0400

    debug: print worker payload

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from terrahaxs_worker.worker import worker, Payload
 
 def run():
     payload = json.loads(os.getenv('PAYLOAD'))
+    print(payload)
     worker(Payload(payload=payload))
 
 if __name__ == '__main__':
